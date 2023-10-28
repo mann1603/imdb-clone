8e8c4e8d7fcb19b5ada4c1cc00b7e88861d252de
Mann Kawatkar <mannkawatkar1603@gmail.com>
2023-10-28 20:18:21 +0530

    First commit without responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Header from "./components/header/Header";
+import Home from "./pages/Home/Home";
+import Movie from "./pages/MovieDetail/Movie";
+import MovieList from "./components/MovieList/MovieList"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="movies/:type" element={<MovieList />}></Route>
+          <Route path="movie/:id" element={<Movie />}></Route>
+          <Route path="/*" element={<h1>Error Page</h1>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
